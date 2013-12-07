b25c794bec85dd256e110d637a63c5c8f9a2edfd
Vikesh Khanna <vikesh@stanford.edu>
2013-12-07 03:17:46 -0800

    Fixing materialize fork bug

--- db/materialize.sql
+++ db/materialize.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS gwatch(userid1, userid2, created_at, score);
 INSERT OR IGNORE INTO gwatch SELECT actor, owner, watch.created_at, 1 from watch, repository where watch.repo_name = repository.name;
 
 CREATE TABLE IF NOT EXISTS gfork(userid1, userid2, created_at, score);
-INSERT OR IGNORE INTO fork SELECT * FROM (SELECT A.actor, B.actor, max(A.created_at, B.created_at) created_at, 0 from fork A, fork B where A.repo_name = B.repo_name and A.actor < B.actor union SELECt actor, owner, fork.created_at, 1 from fork, repository where fork.repo_name = repository.name);
+INSERT OR IGNORE INTO gfork SELECT * FROM (SELECT A.actor, B.actor, max(A.created_at, B.created_at) created_at, 0 from fork A, fork B where A.repo_name = B.repo_name and A.actor < B.actor union SELECt actor, owner, fork.created_at, 1 from fork, repository where fork.repo_name = repository.name);
 
 create table materialized(A);
 insert into materialized values(1);
