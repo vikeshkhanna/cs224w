9e81378ec4f256848a335f30ac0fd6078112de17
Aditya Devdatt Palnitkar <aditpal@corn24.stanford.edu>
2013-12-07 01:23:03 -0800

    adding matlab functions

--- learning/getQ.m
+++ learning/getQ.m
@@ -1,21 +1,13 @@
-% takes in the source node id, the mapping from v to phi(source, v), and the adjacency matrix A. Returns the sparse matrix Q.
-% also takes in the model from the learning phase to calculate f(w, phi(u, v))
+% takes in the source node id, feature matrix F , and the adjacency matrix A, and the model learnt from the learning stage. Returns the sparse matrix Q.
 
-function Q= getQ(fname, beta)
-	load(strcat(fname, "_RWfeatures.mat"));
-	load(strcat(fname, "_model.mat"));
-	% now matrices in memory should be Ai, Aj, Av (sparse matrix vectors), F (feature matrix with the first column being the node IDs for v and rest being features, and s which is the node ID of source node
-	% the mapping from actual IDs to new IDs is: F(newID, 1)= oldID
+function Q= getQ(Ai, Aj, Av, F, src, model)
 	[n_nodes, n_features]= size(F);
-	n_features= n_features-1; 	
-	A= sparse(Ai, Aj, Av);
-	n_edges= size(Ai);
-	% find new ID for source node
-	newSourceID= find(squeeze(F(:, 1))== s);
+	A= sparse(double(Ai), double(Aj), double(Av));
+	n_edges= size(Ai);	% not the actual number of edges
 	% for every other node find f(w, phi(u, v))
 	func= zeros(n_nodes, 1);
 	for i= 1:n_nodes
-		func(i)= calcFunc(squeeze(F(i, 2:end)), model, beta);
+		func(i)= calcFunc(squeeze(F(i, :)), model, beta);
 	end
 	% now given the f(w, phi(s, v)) for every node v, calculate unnormalized values for Q based on A: for every edge (i, j) in A, assign it the weight func(j) 
 	Qv= zeros(n_edges, 1);
@@ -35,5 +27,5 @@ function Q= getQ(fname, beta)
 		Qv(e)= Qv(e)/ normalizer(Ai(e));
 	end
 	
-	Q= sparse(Ai, Aj, Qv);
+	Q= sparse(double(Ai), double(Aj), double(Qv));
 
