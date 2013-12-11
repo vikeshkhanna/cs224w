aeafefe445a6d645c53dde70c82e0eec4af4afa6
Vikesh Khanna <vikesh@stanford.edu>
2013-12-11 01:09:48 -0800

    Adding message to runrw

--- learning/runRW.m
+++ learning/runRW.m
@@ -1,6 +1,7 @@
 % make script accept basename and source vertex ID
 function topIDs = runRW(Ai, Aj, Av, F, src, beta)
-	load('model.k4.mat')
+	disp('Random Walk starteth.')
+	load('model.mat')
 	Q = getQ(Ai, Aj, Av, F, src, beta, model);
 	p= getPageRanks(Q);
 	[sortedp, topIDs]= sort(p, 'descend');
